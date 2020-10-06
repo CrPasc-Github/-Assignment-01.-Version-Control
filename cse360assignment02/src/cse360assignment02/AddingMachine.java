503f8fa0e08a447b82271caa8d13b7a339bf5223
CrPasc-Github <Tinti@LAPTOP-B8JAC1K2.mshome.net>
2020-10-05 18:21:00 -0700

    initial commit

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,57 @@
+package cse360assignment02;
+/**
+ * https://github.com/CrPasc-Github/-Assignment-01.-Version-Control
+ * 
+ * <h1> Adding Machine <h1>
+ * This assignment is designed to practice using a version control system
+ *
+ */
+public class AddingMachine {
+  private int total;
+  
+  /** 
+   * constructor
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /** This is the getTotal method
+   * 
+   *  @return int This returns the current total
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /** This is the add method that adds the parameter to the total variable
+   * 
+   * @param value used
+   */
+  public void add (int value) {
+  }
+
+  /** 
+   * This is the subtract method that subtracts the parameter from the total variable
+   *  
+   * @param value used
+   */
+  public void subtract (int value) {
+  }
+
+  /** 
+   * A history of the transactions must be kept 
+   * to be returned by the toString method
+   * 
+   * @return string This returns the history
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * The clear method should clear the "memory"
+   */
+  public void clear() {
+  }
+}
